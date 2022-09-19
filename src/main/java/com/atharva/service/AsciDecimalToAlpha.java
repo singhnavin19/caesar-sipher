4007062fb851813f59c2704abd7e73fee73c9f02
Navin Singh <singhnavin19>
2022-09-19 22:03:49 +0530

    asci code

---
+++ src/main/java/com/atharva/service/AsciDecimalToAlpha.java
@@ -0,0 +1,54 @@
+package com.atharva.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AsciDecimalToAlpha {
+    public static void main(String[] args) {
+		String input = "";
+		char h;
+		List<Character> outputArray = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+		input = sc.nextLine();
+
+		System.out.println(input);
+		for (String no : input.split(" ")) {
+			int n = Integer.parseInt(no);
+			outputArray.add((char) n);
+		}
+		System.out.println(outputArray);
+        int min = 0;
+        int max = 128;
+        for (int i = min; i < max; i++) {
+            char ch = (char) i;
+            String display = "";
+            if (Character.isWhitespace(ch)) {
+                switch (ch) {
+                    case '\r':
+                        display = "\\r";
+                        break;
+                    case '\t':
+                        display = "\\t";
+                        break;
+                    case '\n':
+                        display = "\\n";
+                        break;
+                    case '\f':
+                        display = "\\f";
+                        break;
+                    case ' ':
+                        display = "space";
+                        break;
+                    default:
+                        display = "whitespace";
+                        break;
+                }
+            } else if (Character.isISOControl(ch)) {
+                display = "control";
+            } else {
+                display = Character.toString(ch);
+            }
+        }
+    }
+}
